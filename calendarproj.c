fbf9375c35423555485d099b13689c153173665b
SicillianoDefensiano <145238012+SicillianoDefensiano@users.noreply.github.com>
2023-12-02 07:59:18 -0500

    calendar print proto

--- calendarproj.c
+++ calendarproj.c
@@ -1,1 +1,29 @@
 #include <stdio.h>
+
+int main(){
+    int day;
+    printf("pick a num from 1 to 30");
+    scanf("%d", &day);
+
+    printf("Month       Year\n");
+    printf("Sun     Mon     Tue     Wed    Thu    Fri    Sat\n");
+
+    for(int i = 0; i < 30; i++){
+        if ((i % 7) == 0 && i != 0)
+            printf("|\n __________________________________________\n\n");
+        if ((i + 1) == day){
+            if ((i + 1) < 10)
+            printf("|%d  UwU", i + 1);
+            else
+            printf("|%d UwU", i + 1);
+        }
+        else{
+            if ((i + 1) < 10)
+            printf("| %d    ", i + 1);
+            else
+            printf("| %d   ", i + 1);
+        }
+        
+    }
+    return 0;
+}
