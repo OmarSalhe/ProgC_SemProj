fe5954ff632adbe3368b7af401e064f0f7f6e93e
SicillianoDefensiano <145238012+SicillianoDefensiano@users.noreply.github.com>
2023-12-05 14:24:35 -0500

    Update calendarproj.c

--- calendarproj.c
+++ calendarproj.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+int zeller(int day, int month, int year);
+
 int main(){
-    int day;
+    int day, month, year;
+
+    printf("%d", zeller(5, 12, 2023));
+    return 0;
+}
+
+int zeller(int day, int month, int year){
+    int K = year % 100, J = (year / 100);
+    return (day + (13 * (month + 1) / 5) + K + (K / 4) + (J / 4) - 2 * J) % 7;
+}
+
+
+
+
+/*
+int day;
     printf("pick a num from 1 to 30");
     scanf("%d", &day);
 
@@ -25,5 +42,4 @@ int main(){
         }
         
     }
-    return 0;
-}
+*/
