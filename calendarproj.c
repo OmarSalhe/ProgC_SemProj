c29ad7218e6e7f8c1c97356f33689fa8faac40bc
SicillianoDefensiano <145238012+SicillianoDefensiano@users.noreply.github.com>
2023-12-08 11:30:17 -0500

    NEVER BOOL

--- calendarproj.c
+++ calendarproj.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <string.h>
 
 // Function to check if a year is a leap year
-bool isLeapYear(int year) {
+int isLeapYear(int year) {
     return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 }
 
